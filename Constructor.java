8b0c13a8234fb957b6f15ca3b07567ec71f5a2af
Abdulsamad <lasisiabdulsamad9@gmail.com>
2024-04-14 19:56:11 +0100

    first commit

---
+++ Constructor.java
@@ -0,0 +1,27 @@
+public class Constructor{
+    double Area;
+
+    Constructor(int length, int breadth){
+        Area = length * breadth;
+        System.out.println("-------------------");
+        System.out.println("The Area of the Rectangle is: " + "\t" + Area + "cm2");
+    }
+
+    Constructor(double base, double height){
+        Area = 0.5 * base * height;
+        System.out.println("The Area of the Traingle is: " + "\t" + Area + "cm2");
+    }
+
+    Constructor(double radius){
+        Area = Math.PI * radius * radius;
+        System.out.println("----------------------");
+        System.out.println("The Area of the Circle is: " + "\t" + Area + "cm2");
+        System.out.println("----------------------");
+    }
+
+    public static void main(String[] args) {
+        new Constructor(30, 60);
+        new Constructor(20.60, 60.30);
+        new Constructor(45.6);
+    }
+}
